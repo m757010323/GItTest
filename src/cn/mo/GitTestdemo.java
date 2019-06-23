850d2c1be819a13a9f6ff8a549b879fe23fee713
habbyppy <35439037+habbyppy@users.noreply.github.com>
2019-06-23 21:08:52 +0800

    Initial commit

---
+++ src/cn/mo/GitTestdemo.java
@@ -0,0 +1,8 @@
+package cn.mo;
+
+public class GitTestdemo {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloGit");
+    }
+}
